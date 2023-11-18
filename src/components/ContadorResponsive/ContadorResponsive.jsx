68a018b8780a708fa9291bf4380a9a365f7e55d9
Dario Rodrigo Torrejon <dartorrejon432@outlook.com>
2023-11-18 04:41:56 -0300

    Aggreglados algunos problemas de responsive

--- src/components/ContadorResponsive/ContadorResponsive.jsx
+++ src/components/ContadorResponsive/ContadorResponsive.jsx
@@ -5,7 +5,7 @@ import BotonContador from "../BotonContador/BotonContador";
 import BotonReset from "../BotonReset/BotonReset";
 const ContadorResponsive = ({ sumar, restar }) => {
     let paso =1
-    let limite = 10
+    let limite = 40
     const [cambiarValor, setCambiarValor] = useState(0)
    
 
@@ -41,21 +41,21 @@ if(cambiarValor > limite){
 }
 
     return (
-        <>
+        <Flex direction='column' justifyContent={["space-around",'space-between']} h={['auto','75dvh']}>
         <chakra.span
         style={{ fontFamily: 'Contador', fontStyle: 'italic' }}
-        fontSize={['8em', '12em']}
+        fontSize={['8em', '12em', '22em']}
         textAlign='center'
         color='#A2A0A0'
         >{cambiarValor}</chakra.span>
-        <Flex direction={['column','row']} py={['20px','1em']} bg={['#1a1a1a','none']} justifyContent={['space-arround','space-evenly']}  alignItems={['center','center']} gap={['10px',null]}>
+        <Flex direction={['column-reverse','row']} py={['25px','1em']} bg={['#1a1a1a','none']} justifyContent={['space-arround','space-evenly']}  alignItems={['center','center']} gap={['10px',null]}>
             <BotonContador operacion='-2' funcOperacion={disminuir} paso={2} valor={cambiarValor} limite={-limite}/>
             <BotonContador operacion={restar} funcOperacion={disminuir} paso={paso}valor={cambiarValor} limite={-limite}/>
             <BotonReset valor={reset}/>
             <BotonContador operacion={sumar} funcOperacion={aumentar} paso={paso} valor={cambiarValor} limite={limite}/>
             <BotonContador operacion='+2' funcOperacion={aumentar} paso={2} valor={cambiarValor} limite={limite}/>
         </Flex>
-        </>
+        </Flex>
     )
 
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,16 @@ import { Box, chakra } from "@chakra-ui/react"
 
 const Footer = () => {
     return (
-        <Box as='footer' color='white' bg={['#1a1a1a','none']} h={['8vh',"12dvh"]}>Yo soy el footer</Box>
+        <Box
+        as='footer' 
+        color='white' 
+        bg={['#1a1a1a','none']} 
+        h={['9vh',"auto"]}
+        position='absolute'
+        bottom='0'
+        width='100dvw'
+        px={['10px','20px']}
+        py={['10px','20px']}>Yo soy el footer</Box>
     )
 }
 
